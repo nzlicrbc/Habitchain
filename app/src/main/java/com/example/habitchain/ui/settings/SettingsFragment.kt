91a365440a8d7a7c8fee658efe7e0ec1c93d7bd2
nzlicrbc <nzlcn10@hotmail.com>
2024-09-09 00:40:49 +0300

    add settings files

---
+++ app/src/main/java/com/example/habitchain/ui/settings/SettingsFragment.kt
@@ -0,0 +1,39 @@
+package com.example.habitchain.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.habitchain.R
+import com.example.habitchain.databinding.FragmentSettingsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    private val viewModel: SettingsViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonLogout.setOnClickListener {
+            viewModel.logout()
+        }
+
+        viewModel.isLoggedOut.observe(viewLifecycleOwner) { isLoggedOut ->
+            if (isLoggedOut) {
+                findNavController().navigate(R.id.action_global_loginFragment)
+            }
+        }
+    }
+}
